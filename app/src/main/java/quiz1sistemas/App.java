0b7918e980e22464c9a4f9528ff49e38c18ea406
Chopeneitor <juanpablohr2006@gmail.com>
2024-08-08 07:58:06 -0500

    Codigo Pablo

--- app/src/main/java/quiz1sistemas/App.java
+++ app/src/main/java/quiz1sistemas/App.java
@@ -7,7 +7,100 @@ public class App {
         // Logica de solucion
         
     }
+public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Bienvenido al cine");
 
+            System.out.println("Por favor ingresar que tipo de boleta va a comprar (G o V):");
+            String tipoBoleta = sc.nextLine();
+
+            System.out.println("¿Cuantas boletas desea comprar?");
+            int cantidadBoletas = sc.nextInt();
+
+            int valorBoletas;
+
+            if (tipoBoleta == "G") {
+
+                valorBoletas = 10000*cantidadBoletas;
+                
+            }else{
+
+                valorBoletas = 15000*cantidadBoletas;
+
+            }
+
+            System.out.println("Que combo desea comprar (1, 2, 3)");
+            int tipoCombo = sc.nextInt();
+
+            System.out.println("Cuantos combos desea comprar");
+            int cantidadCombos = sc.nextInt();
+
+            int valorCombo;
+            int valorCombo1 = 40000;
+            int valorCombo2 = 30000;
+            int valorCombo3 = 42000;
+
+
+            switch (tipoCombo) {
+                case 1:
+                    valorCombo = valorCombo1 * cantidadCombos;
+                    break;
+                case 2:
+                    valorCombo = valorCombo2 * cantidadCombos;
+                    break;
+                case 3:
+                    valorCombo = valorCombo3 * cantidadCombos;
+                    break;
+                default:
+                    valorCombo=0;
+                    break;
+            }
+
+            System.out.println("El valor total de los combos es: " + valorCombo);
+
+            double descuento;
+            
+
+            if (tipoBoleta == "V"){
+                switch (tipoCombo) {
+                    case 1:
+                        descuento = (double) (valorCombo * 0.5);
+                        break;
+                    case 2:
+                        descuento = (double) (valorCombo * 0.6);
+                        break;
+                    case 3:
+                        descuento = (double) (valorCombo * 0.3);
+                        break;
+                    default:
+                        descuento = 0;
+                        break;
+                }
+            }
+            else{
+                descuento = 0;
+            }
+
+            double valorTotal= valorBoletas + valorCombo - descuento;
+
+            System.out.println("Factura");
+
+            System.out.println("Asiento: " + (tipoBoleta == "G" || tipoBoleta == "V" ? "General" : "VIP"));
+            System.out.println("Cantidad de boletas: " + cantidadBoletas);
+            System.out.println("Valor boletas: " + valorBoletas);
+            System.out.println("Combos: " + cantidadCombos + " valor de los combos: " + valorCombo);
+            System.out.println("El descuento fue: " + descuento);
+            System.out.println("El valor total de la factura es: " + valorTotal);
+            
+
+            
+           
+            
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
     /*  Implemente una funcion llamada calcularValorBoleta que cumpla con las siguientes caracteristicas
         * Recibe: 
             - Una cadena de caracteres que representa el tipo de silla comprada (V ó G)
